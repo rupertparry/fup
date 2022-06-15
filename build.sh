5a30615acd749cf9be7c4fabe20c185974343b06
Rupert Parry <rp@rupert.cloud>
2022-06-15 17:09:02 +1000

    Fixed issue with arguments

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-python3 -m build
-PKG=$(ls dist | grep .whl)
-pip3 install dist/$PKG
+# python3 -m build
+# PKG=$(ls dist | grep .whl)
+# pip3 install dist/$PKG
 rm /usr/local/bin/fup || true
-printf "#!/bin/bash\npython3 -m fup" >> /usr/local/bin/fup
+printf "#!/bin/bash\npython3 -m fup \$@" >> /usr/local/bin/fup
 chmod +x /usr/local/bin/fup
